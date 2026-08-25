b0283d85d87adf9ea9343adf2d8078354f1ebab3
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-25 10:15:34 +0600

    nested arrays and Object has been included

---
+++ working_with_nested_array_object.js
@@ -0,0 +1,26 @@
+
+let students = [
+    {
+        Name: "Rubayat",
+        Id: 138,
+        Department:"EEE",
+        grades:[2.75,3.45,3.65,3.54],
+    },
+    {
+        Name: "AKM Nur",
+        Id: 143,
+        Department:"CSE",
+        grades:[2.75,3.45,3.65,3.54],
+    },
+    {
+        Name: "Sadman",
+        Id: 140,
+        Department:"EEE",
+        grades:[2.75,3.45,3.65,3.54],
+    },
+
+]
+
+students[2].grades[2] = 4; // adding something
+const entries = console.log(students[2].grades[2]);
+// console.log(entries);
