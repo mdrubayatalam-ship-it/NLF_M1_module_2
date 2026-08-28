8dc1ffc5636f6aff40912612bc488d275064e02e
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-28 21:05:28 +0600

    Done All Process

--- restaurant.js
+++ restaurant.js
@@ -13,7 +13,8 @@ function discount(price,percentage){
 // VAT
 function finalNetBill(price, vat=15){
     let vatPrice = (price * vat)/100;
-    return vatPrice + price;
+    // return vatPrice + price;
+    return vatPrice;
 }
 
 // formate amount
@@ -30,6 +31,20 @@ function Capitalized(word)
 
 //Process Order
 function processOrder(user,itemPrice,discountCode){
+    console.log(`-------Processing Order for ${Capitalized(user.name)}-------`);
+    
+    const now = new Date();
+    console.log(`Date: ${now}`);
 
+    if(!validEmail(user1.email)) return 0;
+    console.log(`email: ${user1.email}`);
+
+    if(!discount(discountCode) === "NLB26")
+        return 0;
+    console.log(`Product Price: ${itemPrice} BDT \nDiscount Price: ${discount(itemPrice,20)} BDT \nVat: 15% \nTotal Price: ${discount(itemPrice,20) + finalNetBill(itemPrice)} BDT`);
+    
 }
 
+const user1 = {name: "rubayat", email:"rubayat.alam04@gmail.com"};
+
+processOrder(user1,1335,"NLB26");
